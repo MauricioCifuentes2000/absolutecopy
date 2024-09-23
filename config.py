23e5499594894f1b56228564df9e757a4851f702
MauricioCifuentes2000 <maucifuentes00@live.com.mx>
2024-09-23 15:52:34 -0600

    modified:   config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')
